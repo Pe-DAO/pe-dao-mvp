e206aaec327c0aece6e8a5b389a6dc92b0cc6fc9
arthur999999 <arthurhono.contato@gmail.com>
2023-10-14 16:14:03 -0300

    create a rate street input

--- programs/pedao-v0-0-1/src/lib.rs
+++ programs/pedao-v0-0-1/src/lib.rs
@@ -6,10 +6,35 @@ declare_id!("AMuUBpBnpMQzBkYr7JyURsYJKGSNiJYdGMarN9hEmJb7");
 pub mod pedao_v0_0_1 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+   
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+pub struct RateTheStreet{
+    pub author: Pubkey,
+    pub name: String,
+    pub timestamp: i64,
+    pub image_link: String,
+    pub content: String,
+    pub lat: f64,
+    pub lng: f64 
+}
+
+const DISCRIMINATION_LENGTH:usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_NAME_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+const MAX_IMAGE_LINK_LENGTH: usize = 200 * 4;
+const LAT_LENGTH: usize = 8;
+const LNG_LENGTH: usize = 8;
+
+impl  RateTheStreet {
+    const LEN: usize = DISCRIMINATION_LENGTH + PUBLIC_KEY_LENGTH + 
+    TIMESTAMP_LENGTH + STRING_LENGTH_PREFIX + MAX_NAME_LENGTH + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH 
+    + STRING_LENGTH_PREFIX + MAX_IMAGE_LINK_LENGTH + LAT_LENGTH + LAT_LENGTH;
+}
+
+
+
