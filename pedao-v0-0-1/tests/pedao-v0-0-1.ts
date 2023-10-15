5713aeb48715a5a0d3482f4184e51e85fee157c8
arthur999999 <arthurhono.contato@gmail.com>
2023-10-15 16:17:47 -0300

    test finish

--- pedao-v0-0-1/tests/pedao-v0-0-1.ts
+++ pedao-v0-0-1/tests/pedao-v0-0-1.ts
@@ -2,15 +2,24 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { PedaoV001 } from "../target/types/pedao_v0_0_1";
 
-describe("pedao-v0-0-1", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("pedao-v0-0-1 ", () => {
 
-  const program = anchor.workspace.PedaoV001 as Program<PedaoV001>;
+    anchor.setProvider(anchor.AnchorProvider.env());
+    const program = anchor.workspace.PedaoV001 as Program<PedaoV001>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    it("create a rate of a street", async() =>{
+        const rateKeyPair = anchor.web3.Keypair.generate();
+
+        await program.methods.sendARateStreet('Arthur', 'https://i.pinimg.com/564x/56/e9/4d/56e94d97d290465608654b3d34c5f999.jpg', 'Nice place', -23.589254, -46.690015)
+        .accounts({
+            myRate: rateKeyPair.publicKey,
+            senderOfRate: program.provider.publicKey,
+            systemProgram: anchor.web3.SystemProgram.programId
+        }).signers([rateKeyPair])
+        .rpc();
+
+        const rateStreetAccount = await program.account.rateTheStreet.fetch(rateKeyPair.publicKey);
+
+        console.log(rateStreetAccount);
+    })
 });
